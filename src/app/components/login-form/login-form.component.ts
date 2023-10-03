6ae2d60f0ed211cec436a0653546cf82b75e97ac
Filipe Andrade <filipeandrade.work@gmail.com>
2023-10-03 15:38:22 -0300

    simple login screen

---
+++ src/app/components/login-form/login-form.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.css']
+})
+export class LoginFormComponent {
+form = new FormGroup({
+  username: new FormControl('', Validators.required),
+  password: new FormControl('', Validators.required),
+});
+
+onSubmit() {
+  console.warn(this.form.value);
+}
+}
