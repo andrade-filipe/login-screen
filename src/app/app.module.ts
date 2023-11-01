1aed704803a36120d9f78f82f79475412b134650
Filipe Andrade <101667610+andrade-filipe@users.noreply.github.com>
2023-10-31 23:19:06 -0300

    Injected app_config into service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
+import { APP_CONFIG, APP_SERVICE_CONFIG } from './app-config/app-config.service';
 
 @NgModule({
   declarations: [AppComponent, LoginFormComponent, RegisterFormComponent],
@@ -31,7 +32,12 @@ import { MatSelectModule } from '@angular/material/select';
     ReactiveFormsModule,
     MatSelectModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_SERVICE_CONFIG,
+      useValue: APP_CONFIG,
+  },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/service/login/login.service.ts
+++ src/app/service/login/login.service.ts
@@ -1,10 +1,21 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { AppConfig } from 'src/app/app-config/app-config.interface';
+import { APP_SERVICE_CONFIG } from 'src/app/app-config/app-config.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  readonly API_URL: string = `${this.config.apiUrl}/api/v1`;
 
-  constructor(private http: HttpClient) {}
+    httpOptions = {
+        headers: new HttpHeaders({
+            'Content-Type': 'application/json',
+        }),
+    };
+
+  constructor(
+    private http: HttpClient,
+    @Inject(APP_SERVICE_CONFIG) private config: AppConfig) {}
 }
